5436ba9e5f0023aec931418e9a30f7d4fdc35164
tarikaziz0933 <tarikaziz0933@gmail.com>
2022-12-09 19:15:15 +0600

    set height

--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Element.css'
 
 const Element = (props) => {
     console.log(props);
     const { picture, age, name, about, time } = props.element;
     return (
-        <div>
+        <div className='each-element'>
             <img src={picture} alt="" />
             <h3>{name}</h3>
             <p>{about}</p>
