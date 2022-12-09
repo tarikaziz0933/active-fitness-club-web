152ab086affca7be513a00730c92dc01135a6be3
tarikaziz0933 <tarikaziz0933@gmail.com>
2022-12-09 17:52:20 +0600

    added accesories

---
+++ src/components/Accesories/Accesories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Accesories = () => {
+    return (
+        <div>
+            <div className="accesories-elements">
+                <h3>Select today's exercise</h3>
+            </div>
+            <div className="exercise-time">
+
+            </div>
+        </div>
+    );
+};
+
+export default Accesories;
