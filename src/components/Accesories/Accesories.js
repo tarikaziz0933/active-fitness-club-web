5b1c30d5db66a8a981d7097935ab27d490aa86ed
tarikaziz0933 <tarikaziz0933@gmail.com>
2022-12-10 13:07:44 +0600

    work in element and accessories

--- src/components/Accesories/Accesories.js
+++ src/components/Accesories/Accesories.js
@@ -1,15 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import Element from '../Element/Element';
 import './Accesories.css'
+import profilePicture from '../../logo/Tarik_Pic.jpg'
 
 const Accesories = () => {
     const [elements, setElements] = useState([]);
+    const [exerciseTime, setexerciseTime] = useState(0);
 
     useEffect(() => {
         fetch('database.json')
             .then(res => res.json())
             .then(data => setElements(data))
     }, [])
+
+    const handleDashboard = (element) => {
+        const newExerciseTime = element.time;
+        const totalTime = newExerciseTime + exerciseTime;
+        setexerciseTime(totalTime);
+    }
+
     return (
         <div className='accesories-container'>
             <div className="accesories-elements">
@@ -17,11 +26,16 @@ const Accesories = () => {
                     elements.map(element => <Element
                         key={element.id}
                         element={element}
+                        handleDashboard={handleDashboard}
                     ></Element>)
                 }
             </div>
             <div className="exercise-time">
-                <h3>Md. Tarikul Aziz</h3>
+                <div className='profile'>
+                    <img src={profilePicture} alt="" />
+                    <h3>Md. Tarikul Aziz</h3>
+                </div>
+                <h4>Exersise Time: {exerciseTime}s</h4>
             </div>
         </div >
     );
--- src/components/Element/Element.js
+++ src/components/Element/Element.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './Element.css'
 
 const Element = (props) => {
-    console.log(props);
+    const { handleDashboard } = props;
     const { picture, age, name, about, time } = props.element;
     return (
         <div className='each-element'>
             <img src={picture} alt="" />
-            <h3>{name}</h3>
-            <p>{about}</p>
-            <p>For Age: {age}</p>
-            <p>Time Required: {time}s</p>
+            <div className='element-info'>
+                <h3 className='element-name'>{name}</h3>
+                <p>{about}</p>
+                <p><small>For Age: {age}</small></p>
+                <p><small>Time Required: {time}s</small></p>
+            </div>
+            <button onClick={() => handleDashboard(props.element)} className='list-button'>
+                <p>Add to list</p>
+            </button>
         </div>
     );
 };
